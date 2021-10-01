5f7be9d5008e10209ab9477a9d87d9367f666a26
Shwetha K S <sks@sks.remote.csb>
2021-10-01 19:28:00 +0530

    return value is added

---
+++ return.py
@@ -0,0 +1,8 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days *calculation_to_units} {name_of_unit}"
+
+my_var =days_to_units(10)
+print(my_var)
